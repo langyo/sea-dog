fb5f67618a4b879a461c5135a40967b2bd9818ca
langyo <langyo.china@gmail.com>
2019-05-14 17:58:42 +0800

    完成了命令解析器 现在可以正常使用了

--- client/src/socketMessageManager/pluginDashboard.js
+++ client/src/socketMessageManager/pluginDashboard.js
@@ -1,55 +1,45 @@
-let PluginDashboard = {
-    register: function(obj) {
-        const dfs = (left, right) => {
-            for(let i of Object.keys(left)){
-                if(right[i] == undefined) right[i] = left[i];
-                else if(typeof left[i] != 'Object' && left[i] != right[i]) right[i] = left[i];
-                else right[i] = dfs(left[i], right[i]);
-            }
-            return right;
-        }
-        dfs(obj, PluginDashboard.register);
+const diff = (from, to) => {
+    for (let i of Object.keys(from)) {
+        if (to[i] == undefined || typeof from[i] != 'object' && from[i] != to[i]) to[i] = from[i];
+        else if (typeof from[i] != 'function') throw new Error("只允许提供函数！");
+        else to[i] = diff(from[i], to[i]);
+    }
+    return to;
+}
+
+export let PluginDashboard = {
+    register: function (obj) {
+        PluginDashboard.register = diff(obj, PluginDashboard.register);
     },
-    receive: function(obj) {
-        const dfs = (left, right) => {
-            for(let i of Object.keys(left)){
-                if(right[i] == undefined) right[i] = left[i];
-                else if(typeof left[i] != 'Object' && left[i] != right[i]) right[i] = left[i];
-                else right[i] = dfs(left[i], right[i]);
-            }
-            return right;
-        }
-        dfs(obj, PluginDashboard.receive);
+    receive: function (obj) {
+        PluginDashboard.receive = diff(obj, PluginDashboard.receive);
     }
 }
 
 // TEST
-let clipboard = "";
-PluginDashboard.register({
-    'execute': () => console.log("execute"),
-    'clipboard': {
-        'set': (obj) => clipboard = obj,
-        'get': () => console.log(clipboard)
-    }
-});
-PluginDashboard.receive({
-    'data': () => console.log('data')
-});
+// let clipboard = "";
+// PluginDashboard.register({
+//     'execute': () => console.log("execute"),
+//     'clipboard': {
+//         'set': (obj) => clipboard = obj,
+//         'get': () => console.log(clipboard)
+//     }
+// });
+// PluginDashboard.register({
+//     'data': () => console.log('data'),
+//     'execute': () => console.log("execute2")
+// });
 
-let getMessage = cmd => {
+export let getMessage = cmd => {
     let args = cmd.trim().split(' ');
     let type = args.shift();
-    console.log("收到指令 " + type + " ，参数：", args);
 
-    let cmds = args, pos = 0;
     let func = type == 'execute' ? PluginDashboard.register : PluginDashboard.receive;
-    try{
-        for(; cmds.length > 0; func = func[args.shift()], ++pos, console.log("遍历到：", func));
-    }catch(e){
-        console.log("最终结果：", func);
-        func(args.slice(pos));
-        return;
-    }
-    console.log("最终结果：", func);
-    func(args.slice(pos));
+    let arg = args.shift();
+
+    for (; typeof func[arg] == 'object'; console.log(typeof func[arg]), func = func[arg], arg = args.shift())
+        if(func === undefined) throw new Error("不存在这个对象！");
+
+    if(func[arg] === undefined) throw new Error("不存在这个对象！");
+    func[arg].apply(null, args);
 }
